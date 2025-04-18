7516c085987093a5c5ebd5d030369c619a7fef8b
isaturquino <163414038+isaturquino@users.noreply.github.com>
2025-04-18 14:24:25 -0300

    Update lista_arrays.js

--- 1Bimestre/lista_arrays.js
+++ 1Bimestre/lista_arrays.js
@@ -22,12 +22,24 @@ console.log(v);  // [5, 4, 3, 2, 1]
 inverterArraySemReverse(v);
 console.log(v);  // [1, 2, 3, 4, 5]
 
-// EXERCÍCIO 4 
+//  EXERCÍCIO 4 
 let v1 = [10, 20, 30];
 let v2 = v1.slice();
-
 v2[0] = 5;
 v2.push(40);
 
 console.log(v1); // [10, 20, 30]
 console.log(v2); // [5, 20, 30, 40]
+
+//EXERCÍCIO 5 
+const v1 = [1, 2, 3, 4, 5]
+//a) abordagem tradicional
+const v2 = [];
+for (let i = 0; i < v1.length; i++) {
+  v2.push(v1[i] * v1[i]); // ou v1[i] ** 2
+}
+console.log(v2); // Saída: [1, 4, 9, 16, 25]
+
+//b) com o map:
+const v3 = v1.map(function(numero){return numero*numero})
+console.log(v3); // Saída: [1, 4, 9, 16, 25]
