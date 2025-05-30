2ec04ece9330dceabdf766f52d67cfce23c79f99
isaturquino <163414038+isaturquino@users.noreply.github.com>
2025-05-30 09:58:29 -0300

    aula 30-05

---
+++ aula/src/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+function Header(){
+    return(
+        <header className="app-header">
+            <h1>Bem vinda Isa!</h1>
+        </header>
+    )
+}
+export default Header
