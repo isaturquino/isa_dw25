75e3f7e3d558b2453cdf48c5ce1b50196cfc3e9c
isaturquino <163414038+isaturquino@users.noreply.github.com>
2025-06-06 09:12:23 -0300

    aula 06_06

---
+++ 2Bimestre/projeto_06_06/src/App.jsx
@@ -0,0 +1,14 @@
+import Contador from './Contador'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <h1>Exemplo React com useState</h1>
+      <Contador/>
+    </div>
+  )
+ 
+}
+
+export default App
---
+++ 2Bimestre/projeto_06_06/src/Botoes.jsx
@@ -0,0 +1,9 @@
+function Botoes({ onIncrement, onDecrement }){
+    return(
+        <div>
+            <button onClick={onDecrement}>-</button>
+            <button onClick={onIncrement}>+</button>
+        </div>
+    );
+}
+export default Botoes;
