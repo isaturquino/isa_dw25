53644b1f032746cc8e39e023e13d2cb683d8e203
isaturquino <163414038+isaturquino@users.noreply.github.com>
2025-06-09 08:42:37 -0300

    Update Input.jsx

--- 2Bimestre/calculoIMC/src/Input.jsx
+++ 2Bimestre/calculoIMC/src/Input.jsx
@@ -2,8 +2,8 @@
 function Input(){
     return(
         <div>
-            <input type="number" name="" id="" />
-            <input type="number" name="" id="" />
+            <input type="number" name="peso" id="peso" />
+            <input type="number" name="altura" id="altura" />
         </div>
     )
 }
