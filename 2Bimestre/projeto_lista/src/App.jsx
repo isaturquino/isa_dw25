bff8717f5dde554564111d76ce8d9ec4c2a2d263
isaturquino <163414038+isaturquino@users.noreply.github.com>
2025-06-16 08:48:37 -0300

    Update App.jsx

--- 2Bimestre/projeto_lista/src/App.jsx
+++ 2Bimestre/projeto_lista/src/App.jsx
@@ -1,15 +1,35 @@
-import Contador from './TarefaInput'
 import './App.css'
 import TarefaInput from './TarefaInput'
+import TarefaList from './TarefaList'
+import { useState } from 'react'
 
 function App() {
+  const [arr_tarefas, setTarefas]=useState([])
+  const adicionarTarefa = (descricao) => {
+    const novaTarefa = {id: Date.now(), descricao, concluida: false }
+      setTarefas([...arr_tarefas, novaTarefa])
+  }
+
+  const concluirTarefa = (id) => {
+    arr_tarefas.map((tarefa) => 
+      tarefa.id === id ? {...arr_tarefas, concluida: !tarefa.concluida} : tarefa
+  )
+  const removerTarefa = (id) => {
+      setTarefas(arr_tarefas.filter((tarefa)=>tarefa.id!==id))
+  }
+
   return(
     <div>
       <h1>Gerenciar de Tarefas</h1>
-      <TarefaInput/>
+      <TarefaInput onAddTarefa={adicionarTarefa}/>
+      <TarefaList
+      tarefas={arr_tarefas}
+      onConcluir={concluirTarefa}
+      onRemover={removerTarefa}
+      />
     </div>
   )
- 
+}
 }
 
-export default App
+export default App;
