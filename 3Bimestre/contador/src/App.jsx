75b67be8ca89f9a9098c2f862146366296d6eec1
isaturquino <163414038+isaturquino@users.noreply.github.com>
2025-09-05 09:55:45 -0300

    styles

--- 3Bimestre/contador/src/App.jsx
+++ 3Bimestre/contador/src/App.jsx
@@ -1,16 +1,11 @@
 import "./styles/global.css";
 import "./styles/theme.css";
+import Contador from "./components/Contador";
 
 export default function App() {
   return (
     <div>
-      <h1>Contador</h1>
-      <div>0</div>
-      <div>
-        <button>-</button>
-        <button>+</button>
-      </div>
-      <button>Reset</button>
+      <Contador/>
     </div>
   );
 }
