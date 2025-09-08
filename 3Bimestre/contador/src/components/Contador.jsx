2cc7cb41eb5677e380fadf14411f3971b566fe7f
isaturquino <isabelyturquino@alunos.utfpr.edu.br>
2025-09-08 09:18:12 -0300

    funcionando

--- 3Bimestre/contador/src/components/Contador.jsx
+++ 3Bimestre/contador/src/components/Contador.jsx
@@ -1,16 +1,34 @@
-import styles from "./Contador.module.css"
+import styles from"./Contador.module.css"
+
+import  {useState} from "react";
 
 export default function Contador(){
-    console.log(styles);
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.titulo}>Contador</h1>
-            <div className={styles.contadorDisplay}>0</div>
-            <div className={styles.botaoContainer}>
-                <button className={styles.botao}>-</button>
-                <button className={styles.botao}>+</button>
-            </div>
-            <button>Reset</button>
+
+    const [contador, setContador] = useState (0)
+
+    function incrementar (){
+        setContador(contador + 1)
+    }
+
+    function decrementar(){
+        setContador(contador - 1)
+    }
+
+  return(
+    <>
+      <div className={styles.container}>
+        <h1>Contador</h1>
+        <div className={styles.contadorDisplay}>{contador}</div>
+        <div className={styles.botaoContainer}>
+          <button className={styles.botao}
+          onClick={decrementar}
+          >-</button>
+          <button className={styles.botao}
+          onClick={incrementar}
+          >+</button>
         </div>
-    )
+        <button className={styles.botao}> Reset</button>
+      </div>
+    </>
+  )
 }
