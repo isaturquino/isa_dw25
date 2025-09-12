a6abe56f6a1312936e2369065a45ebc8da3ba723
isaturquino <163414038+isaturquino@users.noreply.github.com>
2025-09-12 08:48:04 -0300

    final contador

--- 3Bimestre/contador/src/components/Contador.jsx
+++ 3Bimestre/contador/src/components/Contador.jsx
@@ -1,33 +1,51 @@
 import styles from"./Contador.module.css"
 
-import  {useState} from "react";
+import { useState } from "react";
 
 export default function Contador(){
 
-    const [contador, setContador] = useState (0)
+  // const estado = useState(0)
+  // let contador = estado[0]
+  // const setContador = estado[1]
+  
+  let [contador, setContador] = useState(0)
+  
 
-    function incrementar (){
-        setContador(contador + 1)
-    }
+  function incrementar(){
+    setContador(contador => contador + 1)
+  }
 
-    function decrementar(){
-        setContador(contador - 1)
-    }
+  function decrementar(){
+    setContador(contador => contador - 1)
+  }
+  function reset(){
+    setContador(contador = 0)
+  }
 
+  
   return(
     <>
       <div className={styles.container}>
         <h1>Contador</h1>
-        <div className={styles.contadorDisplay}>{contador}</div>
+        <div className={styles.contadorDisplay}>{ contador }</div>
         <div className={styles.botaoContainer}>
-          <button className={styles.botao}
-          onClick={decrementar}
-          >-</button>
-          <button className={styles.botao}
-          onClick={incrementar}
-          >+</button>
+          <button 
+            className={styles.botao}
+            onClick={ decrementar}
+          >
+            -
+          </button>
+          <button 
+            className={styles.botao}
+            onClick={incrementar}
+          >
+            +
+          </button>
         </div>
-        <button className={styles.botao}> Reset</button>
+        <button className={styles.botao}
+                  onClick={reset}> 
+          Reset
+        </button>
       </div>
     </>
   )
